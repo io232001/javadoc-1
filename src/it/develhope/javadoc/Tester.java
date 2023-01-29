149a5ce5717bfa957d8802348ac2ec4e2cb484a0
iosbd <io232001@gmail.com>
2023-01-29 18:13:02 +0100

    Initial commit

---
+++ src/it/develhope/javadoc/Tester.java
@@ -0,0 +1,12 @@
+package it.develhope.javadoc;
+
+public class Tester
+{
+    public static void main(String args[])
+    {
+        WordGames WordGames=new WordGames();
+
+        System.out.println(WordGames.addHelloWord("Word"));
+        System.out.println(WordGames.getFullName("Dwayne","Jhonson"));
+    }
+}
---
+++ src/it/develhope/javadoc/WordGames.java
@@ -0,0 +1,14 @@
+package it.develhope.javadoc;
+public class WordGames
+{
+    public String addHelloWord(String Word)
+    {
+        return "Hello " + Word;
+    }
+    public String name;
+    public String surname;
+    public String getFullName(String name,String surname)
+    {
+        return name +" "+surname;
+    }
+}
