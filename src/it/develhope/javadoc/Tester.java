821cad2daf2e614313d389225061639a1c10a8c9
iosbd <io232001@gmail.com>
2023-02-27 11:10:37 +0100

    Commit

--- src/it/develhope/javadoc/Tester.java
+++ src/it/develhope/javadoc/Tester.java
@@ -1,11 +1,13 @@
 package it.develhope.javadoc;
-
+/**
+ * Classe Tester che verifica il corretto funzionamento dei metodi della classe WordGames.
+ * Autore: Ionut
+ */
 public class Tester
 {
     public static void main(String args[])
     {
         WordGames WordGames=new WordGames();
-
         System.out.println(WordGames.addHelloWord("Word"));
         System.out.println(WordGames.getFullName("Dwayne","Jhonson"));
     }
--- src/it/develhope/javadoc/WordGames.java
+++ src/it/develhope/javadoc/WordGames.java
@@ -1,12 +1,29 @@
 package it.develhope.javadoc;
+/**
+ * Classe WordGames che contiene due metodi per manipolare le parole.
+ * Autore: Ionut
+ */
 public class WordGames
 {
+    /**
+     * Aggiunge "Hello " davanti alla parola inserita come parametro.
+     *
+     * @param word la parola a cui aggiungere "Hello"
+     * @return la stringa "Hello " seguita da word
+     */
     public String addHelloWord(String Word)
     {
         return "Hello " + Word;
     }
     public String name;
     public String surname;
+    /**
+     * Concatena il nome e il cognome inseriti come parametro.
+     *
+     * @param name il nome da concatenare
+     * @param surname il cognome da concatenare
+     * @return la stringa ottenuta è la concatenazione tra il nome e il cognome inseriti come parametro
+     */
     public String getFullName(String name,String surname)
     {
         return name +" "+surname;
